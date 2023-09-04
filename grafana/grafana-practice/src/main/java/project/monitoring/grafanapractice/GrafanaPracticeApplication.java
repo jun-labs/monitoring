7fe433d41f860ff5a83f90c3b9ea00d3f4dad52c
devjun10 <devjun1023@gmail.com>
2023-09-05 03:30:44 +0900

    feat: add init data setting

--- grafana/grafana-practice/src/main/java/project/monitoring/grafanapractice/GrafanaPracticeApplication.java
+++ grafana/grafana-practice/src/main/java/project/monitoring/grafanapractice/GrafanaPracticeApplication.java
@@ -1,13 +1,40 @@
 package project.monitoring.grafanapractice;
 
+import static project.monitoring.grafanapractice.common.auth.AppUser.appUser;
+import jakarta.annotation.PostConstruct;
+import java.math.BigDecimal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import project.monitoring.grafanapractice.core.domain.product.entity.Product;
+import project.monitoring.grafanapractice.core.domain.product.repository.ProductJpaRepository;
+import project.monitoring.grafanapractice.core.domain.user.entity.User;
+import project.monitoring.grafanapractice.core.domain.user.repository.UserJpaRepository;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GrafanaPracticeApplication {
+
+    private final UserJpaRepository userJpaRepository;
+    private final ProductJpaRepository productJpaRepository;
+
+    public GrafanaPracticeApplication(
+        UserJpaRepository userJpaRepository,
+        ProductJpaRepository productJpaRepository
+    ) {
+        this.userJpaRepository = userJpaRepository;
+        this.productJpaRepository = productJpaRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GrafanaPracticeApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        userJpaRepository.save(new User(appUser.getRole()));
+        productJpaRepository.save(
+            new Product("독서실 한 달 이용권", BigDecimal.valueOf(100_000L))
+        );
+    }
 }
