cd33082e5161314ade70f6f9b796d3448b7d7146
devjun10 <devjun1023@gmail.com>
2023-09-05 03:35:25 +0900

    feature: add web configuration

---
+++ grafana/grafana-practice/src/main/java/project/monitoring/grafanapractice/common/configuration/web/WebConfiguration.java
@@ -0,0 +1,22 @@
+package project.monitoring.grafanapractice.common.configuration.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8086")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
